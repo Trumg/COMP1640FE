ac5b255c1cc5de1ddb196be6cd3dc690b2e9e21c
mchlphn <michaelphan1703@gmail.com>
2024-03-15 21:58:17 +0700

    Update login page

--- src/Components/Header/AppHeader/AppHeader.tsx
+++ src/Components/Header/AppHeader/AppHeader.tsx
@@ -51,7 +51,7 @@ function AppHeader() {
                 color: "inherit",
               }}
             >
-              LOGIN
+              JOIN
             </Link>
           </Button>
         </Toolbar>
