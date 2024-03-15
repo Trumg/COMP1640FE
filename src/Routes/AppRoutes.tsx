f3753e022d47d42fba8030a8d072357b299a4bae
mchlphn <michaelphan1703@gmail.com>
2024-03-15 22:32:15 +0700

    Update

--- src/Routes/AppRoutes.tsx
+++ src/Routes/AppRoutes.tsx
@@ -3,12 +3,18 @@ import { Route, Routes } from "react-router-dom";
 
 import HomePage from "../Pages/HomePage/HomePage";
 import LoginPage from "../Pages/LoginPage/LoginPage";
+import UserPage from "../Pages/UserPage/UserPage";
+import ManagerPage from "../Pages/ManagerPage/ManagerPage";
+import AdminPage from "../Pages/AdminPage/AdminPage";
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="auth/login" element={<LoginPage />} />
+      <Route path="/user" element={<UserPage />} />
+      <Route path="/auth/login" element={<LoginPage />} />
+      <Route path="/dashboard/manager" element={<ManagerPage />} />
+      <Route path="/dashboard/admin" element={<AdminPage />} />
     </Routes>
   );
 };
