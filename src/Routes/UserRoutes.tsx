85a92fe6cf6cea0d0f8a5bb61cd19e0ec0f6a0d8
mchlphn <michaelphan1703@gmail.com>
2024-03-17 12:24:33 +0700

    Fix duplicate login form

--- src/Routes/UserRoutes.tsx
+++ src/Routes/UserRoutes.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+
 import UserPage from "../Pages/UserPage/UserPage";
-import ProfilePage from "../Pages/UserPage/ProfilePage";
-import SettingPage from "../Pages/UserPage/SettingPage";
 
 const UserRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/user" element={<UserPage />} />
-      <Route path="/user/profile" element={<ProfilePage />} />
-      <Route path="/user/settings" element={<SettingPage />} />
     </Routes>
   );
 };
