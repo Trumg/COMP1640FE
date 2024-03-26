40c7f06d1e7153fdfeddb72107081d3ca4743bde
mchlphn <michaelphan1703@gmail.com>
2024-03-26 23:55:34 +0700

    Update

--- src/Components/Navbar/GuestNavbar/GuestNavbar.tsx
+++ src/Components/Navbar/GuestNavbar/GuestNavbar.tsx
@@ -3,12 +3,13 @@ import { MagazineImage } from "../../../Assets/MagazineImage/MagazineImage";
 import { Link } from "react-router-dom";
 import AuthModalDesktop from "../../Modal/AuthModal/AuthModalDesktop";
 import AuthModalMobile from "../../Modal/AuthModal/AuthModalMobile";
-import { FaSearch, FaBars, FaTimes, FaMoon, FaSun } from "react-icons/fa";
+import { FaSearch, FaBars, FaTimes } from "react-icons/fa";
+import { IoMoon, IoSunny } from "react-icons/io5"; // Import moon and sun icons
 
 const Navbar: React.FC = () => {
   const [isMobile, setIsMobile] = useState<boolean>(false);
   const [showMenu, setShowMenu] = useState<boolean>(false);
-  const [darkTheme, setDarkTheme] = useState<boolean>(false); // State for dark theme
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -24,14 +25,14 @@ const Navbar: React.FC = () => {
     setShowMenu(!showMenu);
   };
 
-  const toggleDarkTheme = () => {
-    setDarkTheme(!darkTheme); // Toggle dark theme state
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    // You can save the user's preference in localStorage or a state management solution
+    // localStorage.setItem('darkMode', JSON.stringify(!isDarkMode));
   };
 
   return (
-    <nav
-      className={`bg-white shadow-lg w-full fixed ${darkTheme ? "dark" : ""}`}
-    >
+    <nav className="bg-white shadow-lg w-full fixed">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           <div className="flex-shrink-0 py-2">
@@ -66,22 +67,24 @@ const Navbar: React.FC = () => {
                     <input
                       type="text"
                       placeholder="Search..."
-                      className={`bg-white border border-gray-600 rounded-full py-2 pl-10 pr-4 focus:outline-none focus:ring-1 focus:ring-gray-400 shadow-md w-full md:w-96 ${
-                        darkTheme ? "dark" : ""
-                      }`}
+                      className="bg-white border border-gray-600 rounded-full py-2 pl-10 pr-4 focus:outline-none focus:ring-1 focus:ring-gray-400 shadow-md w-full md:w-96"
                     />
                   </div>
                 </div>
               </div>
               <div className="hidden md:flex items-center">
                 <button
-                  onClick={toggleDarkTheme}
-                  className="ml-4 p-2 focus:outline-none"
+                  onClick={toggleDarkMode}
+                  className="flex items-center space-x-2 rounded-full px-3 py-1 text-gray-500 hover:text-gray-800 focus:outline-none focus:text-gray-800"
                 >
-                  {darkTheme ? (
-                    <FaSun className="h-6 w-6 text-yellow-500" />
+                  {isDarkMode ? (
+                    <>
+                      <IoMoon className="h-6 w-6" />
+                    </>
                   ) : (
-                    <FaMoon className="h-6 w-6 text-gray-500" />
+                    <>
+                      <IoSunny className="h-6 w-6" />
+                    </>
                   )}
                 </button>
                 <div className="ml-10 space-x-2">
