3d8a9661a8faa7dff0e2209f71ad1559044358aa
mchlphn <michaelphan1703@gmail.com>
2024-03-26 23:44:17 +0700

    Add toggle dark theme

--- src/Components/Navbar/GuestNavbar/GuestNavbar.tsx
+++ src/Components/Navbar/GuestNavbar/GuestNavbar.tsx
@@ -3,11 +3,12 @@ import { MagazineImage } from "../../../Assets/MagazineImage/MagazineImage";
 import { Link } from "react-router-dom";
 import AuthModalDesktop from "../../Modal/AuthModal/AuthModalDesktop";
 import AuthModalMobile from "../../Modal/AuthModal/AuthModalMobile";
-import { FaSearch, FaBars, FaTimes } from "react-icons/fa"; // Import icons
+import { FaSearch, FaBars, FaTimes, FaMoon, FaSun } from "react-icons/fa";
 
 const Navbar: React.FC = () => {
   const [isMobile, setIsMobile] = useState<boolean>(false);
   const [showMenu, setShowMenu] = useState<boolean>(false);
+  const [darkTheme, setDarkTheme] = useState<boolean>(false); // State for dark theme
 
   useEffect(() => {
     const handleResize = () => {
@@ -23,8 +24,14 @@ const Navbar: React.FC = () => {
     setShowMenu(!showMenu);
   };
 
+  const toggleDarkTheme = () => {
+    setDarkTheme(!darkTheme); // Toggle dark theme state
+  };
+
   return (
-    <nav className="bg-white shadow-lg w-full fixed">
+    <nav
+      className={`bg-white shadow-lg w-full fixed ${darkTheme ? "dark" : ""}`}
+    >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           <div className="flex-shrink-0 py-2">
@@ -59,12 +66,24 @@ const Navbar: React.FC = () => {
                     <input
                       type="text"
                       placeholder="Search..."
-                      className="bg-white border border-gray-600 rounded-full py-2 pl-10 pr-4 focus:outline-none focus:ring-1 focus:ring-gray-400 shadow-md w-full md:w-96"
+                      className={`bg-white border border-gray-600 rounded-full py-2 pl-10 pr-4 focus:outline-none focus:ring-1 focus:ring-gray-400 shadow-md w-full md:w-96 ${
+                        darkTheme ? "dark" : ""
+                      }`}
                     />
                   </div>
                 </div>
               </div>
               <div className="hidden md:flex items-center">
+                <button
+                  onClick={toggleDarkTheme}
+                  className="ml-4 p-2 focus:outline-none"
+                >
+                  {darkTheme ? (
+                    <FaSun className="h-6 w-6 text-yellow-500" />
+                  ) : (
+                    <FaMoon className="h-6 w-6 text-gray-500" />
+                  )}
+                </button>
                 <div className="ml-10 space-x-2">
                   <AuthModalDesktop />
                 </div>
