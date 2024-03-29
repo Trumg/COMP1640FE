00a655fc494b0ecec9f28ac30acb3f2237127d4f
mchlphn <michaelphan1703@gmail.com>
2024-03-29 19:43:28 +0700

    Update

--- src/Pages/UserPage/UserPage.tsx
+++ src/Pages/UserPage/UserPage.tsx
@@ -7,14 +7,25 @@ const { Content } = Layout;
 const App: React.FC = () => {
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <UserNavbar />
-      <Content style={{ padding: "24px 48px 0", flexGrow: 1 }}>
+      <div style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+        <UserNavbar />
+      </div>
+      <Content
+        style={{
+          padding: "24px 48px 0",
+          flexGrow: 1,
+          marginTop: "69px", // Adjusted for the height of the navbar
+          overflowY: "auto", // Enable vertical scrolling
+        }}
+      >
         <div
           style={{
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
-            minHeight: "calc(100vh - 69px - 48px - 48px)", // Adjusted for padding
+            minHeight: "calc(100vh - 69px - 48px - 48px)", // Adjusted for padding and navbar height
+            position: "relative", // Needed for z-index to work
+            zIndex: 0, // Ensure content is below the navbar
           }}
         >
           <Row gutter={16}>
