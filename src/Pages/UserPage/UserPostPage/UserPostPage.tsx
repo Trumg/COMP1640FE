1c209811f6e11fb5be8094c81cd77cb4a28547f3
mchlphn <michaelphan1703@gmail.com>
2024-03-31 17:15:48 +0700

    Update

--- src/Pages/UserPage/UserPostPage/UserPostPage.tsx
+++ src/Pages/UserPage/UserPostPage/UserPostPage.tsx
@@ -1,35 +1,72 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Layout, Card, Input } from "antd";
 import UserPostNavbar from "../../../Components/Navbar/UserNavbar/UserPostNavbar";
 
 const { Content } = Layout;
 
 const UserPostPage: React.FC = () => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "transparent";
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
+  const handleTextareaResize = () => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height =
+        textareaRef.current.scrollHeight + "px";
+    }
+  };
+
   return (
     <Layout>
       <UserPostNavbar />
       <Content
         style={{
-          padding: "50px",
-          marginTop: "50px",
           display: "flex",
           justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "white",
+          paddingTop: "150px",
         }}
       >
-        <div className="max-w-md w-full">
-          <Card className="shadow-md rounded-md">
+        <Card
+          className="shadow-md rounded-md"
+          style={{
+            width: "100%",
+            maxWidth: "1000px",
+            maxHeight: "100%",
+          }}
+        >
+          <div>
+            <Input.TextArea
+              ref={textareaRef}
+              placeholder="Enter title"
+              className="border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              autoSize={{ minRows: 1, maxRows: 1 }}
+              style={{ marginBottom: "20px" }}
+              onInput={handleTextareaResize}
+            />
+          </div>
+          <div>
             <Input.TextArea
               placeholder="Write your post here"
-              autoSize={{ minRows: 4, maxRows: 6 }}
+              autoSize={{ minRows: 15, maxRows: 20 }}
               className="border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              style={{
+                width: "100%",
+                minHeight: "100px",
+                maxHeight: "calc(100vh - 200px)",
+                overflowY: "auto",
+                marginBottom: "20px",
+              }}
             />
-            <div className="flex justify-end mt-2">
-              <button className="bg-[#549b90] border-1 border-black hover:bg-gray-400 font-bold py-2 px-4 rounded-full shadow-md text-sm">
-                Post
-              </button>
-            </div>
-          </Card>
-        </div>
+          </div>
+        </Card>
       </Content>
     </Layout>
   );
