5d858385cdd5a33222eb39421a883dbf76585b8b
mchlphn <michaelphan1703@gmail.com>
2024-03-31 17:23:56 +0700

    Update

--- src/Pages/UserPage/UserPostPage/UserPostPage.tsx
+++ src/Pages/UserPage/UserPostPage/UserPostPage.tsx
@@ -32,6 +32,7 @@ const UserPostPage: React.FC = () => {
           alignItems: "center",
           backgroundColor: "white",
           paddingTop: "150px",
+          paddingBottom: "20px", // Add padding to create space between the button and content area
         }}
       >
         <Card
@@ -40,21 +41,20 @@ const UserPostPage: React.FC = () => {
             width: "100%",
             maxWidth: "1000px",
             maxHeight: "100%",
+            position: "relative", // Required for relative positioning
           }}
         >
-          <div>
+          <div style={{ zIndex: 1 }}>
             <Input.TextArea
               ref={textareaRef}
-              placeholder="Enter title"
+              placeholder="Title"
               className="border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
               autoSize={{ minRows: 1, maxRows: 1 }}
               style={{ marginBottom: "20px" }}
               onInput={handleTextareaResize}
             />
-          </div>
-          <div>
             <Input.TextArea
-              placeholder="Write your post here"
+              placeholder="Content"
               autoSize={{ minRows: 15, maxRows: 20 }}
               className="border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
               style={{
@@ -62,9 +62,14 @@ const UserPostPage: React.FC = () => {
                 minHeight: "100px",
                 maxHeight: "calc(100vh - 200px)",
                 overflowY: "auto",
-                marginBottom: "20px",
+                marginBottom: "20px", // Add margin to create space between content input and button
               }}
             />
+          </div>
+          <div>
+            <button className="bg-[#549b90] border-1 border-black hover:bg-gray-400 font-bold py-2 px-4 rounded-full shadow-md">
+              Post
+            </button>
           </div>
         </Card>
       </Content>
