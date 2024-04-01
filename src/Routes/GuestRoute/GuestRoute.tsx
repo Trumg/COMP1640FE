ba234c9039595c7ca4c7d1b9ee3fb08ad6179193
mchlphn <michaelphan1703@gmail.com>
2024-04-01 09:41:00 +0700

    Update

--- src/Routes/GuestRoute/GuestRoute.tsx
+++ src/Routes/GuestRoute/GuestRoute.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import GuestPage from "../../Pages/GuestPage/GuestPage";
-import { useGetInfoUser } from "../../Hooks/useToken";
 import UserPage from "../../Pages/UserPage/UserPage";
+import { useGetInfoUser } from "../../Hooks/useToken";
 
 const GuestRoute: React.FC = () => {
   const infoUser = useGetInfoUser();
 
   return (
     <Routes>
-      <Route path="/" element={infoUser?.uid ? <UserPage /> : <GuestPage />} />
+      <Route path="/" element={infoUser ? <UserPage /> : <GuestPage />} />
     </Routes>
   );
 };
