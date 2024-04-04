75e06f4b8456bfc504cbedc41012e07e29374ba5
michaelphan1703 <michaelphan1703@gmail.com>
2024-04-05 03:03:09 +0700

    Update

--- src/Pages/StudentPage/StudentPage.tsx
+++ src/Pages/StudentPage/StudentPage.tsx
@@ -1,11 +1,23 @@
-import { useState } from "react";
-import StudentNavbar from "../../Components/Navbar/StudentNavbar";
-import { Card, Button, Form, Input, Upload, Checkbox } from "antd";
+import React, { useState } from "react";
+import {
+  Card,
+  Button,
+  Form,
+  Input,
+  Upload,
+  Checkbox,
+  Tabs,
+  Layout,
+} from "antd";
 import { UploadOutlined } from "@ant-design/icons";
 import { UploadChangeParam } from "antd/lib/upload/interface";
 import { CheckboxChangeEvent } from "antd/lib/checkbox";
+import StudentNavbar from "../../Components/Navbar/StudentNavbar";
 
 const { TextArea } = Input;
+const { TabPane } = Tabs;
+const { Content } = Layout;
+const { Dragger } = Upload;
 
 function StudentPage() {
   const [step, setStep] = useState(1);
@@ -38,79 +50,93 @@ function StudentPage() {
     console.log("Form submitted!");
   };
 
-  const renderFormStep = () => {
-    switch (step) {
-      case 1:
-        return (
-          <Form onFinish={() => setStep(2)}>
-            <Form.Item
-              label="Title"
-              name="title"
-              rules={[{ required: true, message: "Please input title!" }]}
-            >
-              <Input value={title} onChange={handleTitleChange} />
-            </Form.Item>
-            <Form.Item
-              label="Content"
-              name="content"
-              rules={[{ required: true, message: "Please input content!" }]}
-            >
-              <TextArea value={content} onChange={handleContentChange} />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Next
-              </Button>
-            </Form.Item>
-          </Form>
-        );
-      case 2:
-        return (
-          <Form onFinish={() => setStep(3)}>
-            <Form.Item label="Image or Document">
-              <Upload onChange={handleImageChange} showUploadList={false}>
-                <Button icon={<UploadOutlined />}>Click to upload</Button>
-              </Upload>
-            </Form.Item>
-            <Form.Item>
-              <Button type="default" onClick={() => setStep(1)}>
-                Previous
-              </Button>
-              <Button type="primary" htmlType="submit">
-                Next
-              </Button>
-            </Form.Item>
-          </Form>
-        );
-      case 3:
-        return (
-          <Form onFinish={handleSubmit}>
-            <Form.Item>
-              <Checkbox checked={termsAgreed} onChange={handleTermsAgreeChange}>
-                I agree to the terms and conditions
-              </Checkbox>
-            </Form.Item>
-            <Form.Item>
-              <Button type="default" onClick={() => setStep(2)}>
-                Previous
-              </Button>
-              <Button type="primary" htmlType="submit" disabled={!termsAgreed}>
-                Submit
-              </Button>
-            </Form.Item>
-            {image && <img src={URL.createObjectURL(image)} alt="Uploaded" />}
-          </Form>
-        );
-      default:
-        return null;
-    }
-  };
-
   return (
-    <div>
+    <Layout style={{ minHeight: "100vh" }}>
       <StudentNavbar />
-      <Card>{renderFormStep()}</Card>
-    </div>
+      <Content
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "50px",
+        }}
+      >
+        <Card style={{ width: "80%", maxWidth: "800px" }}>
+          <Tabs defaultActiveKey="1" activeKey={String(step)}>
+            <TabPane tab="Step 1" key="1">
+              <Form onFinish={() => setStep(2)}>
+                <Form.Item
+                  label="Title"
+                  name="title"
+                  rules={[{ required: true, message: "Please input title!" }]}
+                >
+                  <Input value={title} onChange={handleTitleChange} />
+                </Form.Item>
+                <Form.Item
+                  label="Content"
+                  name="content"
+                  rules={[{ required: true, message: "Please input content!" }]}
+                >
+                  <TextArea value={content} onChange={handleContentChange} />
+                </Form.Item>
+                <Form.Item>
+                  <button>Next</button>
+                </Form.Item>
+              </Form>
+            </TabPane>
+            <TabPane tab="Step 2" key="2">
+              <Form onFinish={() => setStep(3)}>
+                <Form.Item label="Image or Document">
+                  <Dragger
+                    onChange={handleImageChange}
+                    fileList={[]}
+                    showUploadList={false}
+                  >
+                    <p className="ant-upload-drag-icon">
+                      <UploadOutlined />
+                    </p>
+                    <p className="ant-upload-text">
+                      Click or drag file to this area to upload
+                    </p>
+                  </Dragger>
+                </Form.Item>
+                <Form.Item>
+                  <button onClick={() => setStep(1)}>Previous</button>
+                  <button>Next</button>
+                </Form.Item>
+              </Form>
+            </TabPane>
+            <TabPane tab="Step 3" key="3">
+              <Form onFinish={handleSubmit}>
+                <Form.Item>
+                  <Checkbox
+                    checked={termsAgreed}
+                    onChange={handleTermsAgreeChange}
+                  >
+                    I agree to the terms and conditions
+                  </Checkbox>
+                </Form.Item>
+                <Form.Item>
+                  <Button type="default" onClick={() => setStep(2)}>
+                    Previous
+                  </Button>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    disabled={!termsAgreed}
+                  >
+                    Submit
+                  </Button>
+                </Form.Item>
+                {image && (
+                  <img src={URL.createObjectURL(image)} alt="Uploaded" />
+                )}
+              </Form>
+            </TabPane>
+          </Tabs>
+        </Card>
+      </Content>
+    </Layout>
   );
 }
 
